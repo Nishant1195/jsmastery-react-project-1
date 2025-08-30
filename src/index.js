e941a280e7c8239752b406060663cb8fc228a337
Nishant1195 <nishant8830875196@gmail.com>
2025-08-30 14:53:32 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Create root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render App
+root.render(<App />);
